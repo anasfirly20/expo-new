b838a5d5c3f15f742aa74235aa6411b8012db8fa
Anas Firly <anasfirly20@gmail.com>
2025-01-31 11:48:14 +0300

    refactor: moving app to src

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,15 @@
+process.env.EXPO_ROUTER_APP_ROOT = '../../src/app';
+
 module.exports = function (api) {
     api.cache(true);
     return {
       presets: [
         ["babel-preset-expo", { jsxImportSource: "nativewind" }],
         "nativewind/babel",
+      ],
+      plugins: [
+        ['module:react-native-dotenv'],
+        require.resolve('expo-router/babel'),
       ],
     };
   };
